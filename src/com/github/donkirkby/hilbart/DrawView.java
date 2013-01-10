452b3c9ded824d6eac2a535d9206b74269cd738e
Don Kirkby <donkirkby@gmail.com>
2013-01-09 21:19:35 -0800

    Use the general pattern of n x m cells, and switch to the most recent photo.

--- src/com/github/donkirkby/hilbart/DrawView.java
+++ src/com/github/donkirkby/hilbart/DrawView.java
@@ -34,7 +34,7 @@ public class DrawView extends View {
         super(context);
         paint.setColor(Color.BLACK);
         paint.setStyle(Style.STROKE);
-        gap = 4;
+        gap = 2;
         paint.setStrokeWidth((float) (gap/2.0));
         bitmap = BitmapFactory.decodeFile(getLatestImage());
         path = new Path();
@@ -45,14 +45,18 @@ public class DrawView extends View {
     	if (path.isEmpty()) {
     		int width;
     		int height;
-    		float aspect = bitmap.getHeight() / (float)bitmap.getWidth();
-    		float potentialHeight = canvas.getWidth() * aspect;
-    		if (potentialHeight > canvas.getHeight()) {
-				height = Math.round(canvas.getHeight() / (float)gap);
-				width = Math.round(canvas.getHeight() / aspect / gap);
+    		int imageHeight = bitmap.getHeight();
+			int imageWidth = bitmap.getWidth();
+			float aspect = imageHeight / (float)imageWidth;
+    		int canvasWidth = canvas.getWidth();
+			float potentialHeight = canvasWidth * aspect;
+    		int canvasHeight = canvas.getHeight();
+			if (potentialHeight > canvasHeight) {
+				height = Math.round(canvasHeight / (float)gap);
+				width = Math.round(canvasHeight / aspect / gap);
 			} else {
-				width = Math.round(canvas.getWidth() / (float)gap);
-				height = Math.round(canvas.getWidth() * aspect / gap);
+				width = Math.round(canvasWidth / (float)gap);
+				height = Math.round(canvasWidth * aspect / gap);
 			}
     		Bitmap sized = Bitmap.createScaledBitmap(
     				bitmap, 
@@ -61,28 +65,108 @@ public class DrawView extends View {
     				false);
     		
 			scaled = toGrayscale(sized);
-			int pathWidth = 1;
-			int pathHeight = 2;
+			int n=4;
+			int m=6;
+			int pathWidth = m;
+			int pathHeight = n;
 			int levelCount = 1;
+			int size = 1;
 			
 			while ((pathWidth * 2 < width) && (pathHeight * 2 < height)) {
 				pathWidth *= 2;
 				pathHeight *= 2;
+				size *= 2;
 				levelCount++;
 			}
 			pathWidth *= gap;
 			pathHeight *= gap;
 			calculateLevels(levelCount, 0, 0, pathWidth, pathHeight);
-			path.moveTo(0, pathHeight/2-gap);
-	    	drawCell(0, pathHeight/2-gap, pathWidth-gap, pathHeight/2-gap, 1);
-	    	path.lineTo(pathWidth-gap, pathHeight/2);
-	    	drawCell(pathWidth-gap, pathHeight/2, 0, pathHeight/2, 1);
-	    	path.close();
+			drawNxMCells(pathHeight, pathWidth, n, m, size);
     	}	    	
 	    	
     	canvas.drawPath(path, paint);
     }
 
+	private void draw2x1cells(int pathHeight, int pathWidth) {
+		path.moveTo(0, pathHeight/2-gap);
+		drawCell(0, pathHeight/2-gap, pathWidth-gap, pathHeight/2-gap, 1);
+		path.lineTo(pathWidth-gap, pathHeight/2);
+		drawCell(pathWidth-gap, pathHeight/2, 0, pathHeight/2, 1);
+		path.close();
+	}
+	
+	private void extendPath(int x, int y) {
+		path.lineTo(x * gap, y * gap);
+	}
+	
+	private void movePath(int x, int y) {
+		path.moveTo(x * gap, y * gap);
+	}
+	
+	private void drawNxMCells(
+			int pathHeight, 
+			int pathWidth, 
+			int n, 
+			int m, 
+			int size) {
+		for (int i = 0; i < n/2 - 1; i++)
+		{
+			extendPath(0, (n/2 - i)*size - 1);
+			drawCell(0, (n/2 - i)*size - 1, 0, (n/2 - i - 1)*size, -1);
+		}
+
+		for (int i = 0; i < m; i++)
+		{
+			extendPath(i*size, size-1);
+			drawCell(i*size, size - 1, (i+1)*size - 1, size - 1, 1);
+		}
+
+		for (int i = 0; i < n/2 - 1; i++)
+		{
+			for (int j = 0; j < m-2; j++)
+			{
+                extendPath((m-j)*size - 1, (2*i+1)*size);
+                drawCell(
+                    (m-j)*size - 1, (2*i+1)*size, 
+                    (m-j-1)*size, (2*i+1)*size, 
+                    1);
+			}
+            for (int j = 0; j < 2; j++)
+            {
+                extendPath(2*size - 1, (2*i+j+1)*size);
+                drawCell(
+                    2*size - 1, (2*i+j+1)*size, 
+                    2*size - 1, (2*i+j+2)*size - 1, 
+                    -1);
+            }
+            for (int j = 0; j < m-2; j++)
+            {
+                extendPath((2+j)*size, (2*i+3)*size - 1);
+                drawCell(
+                    (2+j)*size, (2*i+3)*size - 1, 
+                    (3+j)*size-1, (2*i+3)*size - 1, 
+                    1);
+            }
+		}
+		
+		for (int i = 0; i < m; i++)
+		{
+            extendPath((m-i)*size-1, (n-1)*size);
+            drawCell(
+                (m-i)*size-1, (n-1)*size, 
+                (m-i-1)*size, (n-1)*size, 
+                1);
+		}
+		
+		for (int i = 0; i < n/2 - 1; i++)
+		{
+            extendPath(0, (n - i - 1)*size - 1);
+            drawCell(0, (n - i - 1)*size - 1, 0, (n - i - 2)*size, -1);
+		}
+		
+		path.close();
+	}
+		
     private Bitmap toGrayscale(Bitmap bmpOriginal)
     {        
         int width, height;
@@ -120,7 +204,7 @@ public class DrawView extends View {
         int x3 = xOut + dy*sign;
         int y3 = yOut - dx*sign;
         
-        int size = Math.max(Math.abs(dx), Math.abs(dy)) + gap;
+        int size = Math.max(Math.abs(dx), Math.abs(dy)) + 1;
         int xMin = Math.min(Math.min(xIn, x2), Math.min(x3, xOut));
         int xMax = xMin + size;
         int yMin = Math.min(Math.min(yIn, y2), Math.min(y3, yOut));
@@ -143,38 +227,38 @@ public class DrawView extends View {
 		}
         if (intensity >= minIntensity)
     	{
-        	path.lineTo(xOut, yOut);
+        	extendPath(xOut, yOut);
     	}
         else
         {
             int halfSize = size / 2;
             int step_dx = (int) Math.signum(dx);
             int step_dy = (int) Math.signum(dy);
-            int half_dx = (halfSize-gap) * step_dx;
-            int half_dy = (halfSize-gap) * step_dy;
+            int half_dx = (halfSize-1) * step_dx;
+            int half_dy = (halfSize-1) * step_dy;
             int xIn1 = xIn;
             int yIn1 = yIn;
             int xOut1 = xIn + half_dy*sign;
             int yOut1 = yIn - half_dx*sign;
-            int xIn2 = xOut1 + step_dy*sign*gap;
-            int yIn2 = yOut1 - step_dx*sign*gap;
+            int xIn2 = xOut1 + step_dy*sign*1;
+            int yIn2 = yOut1 - step_dx*sign*1;
             int xOut2 = xIn2 + half_dx;
             int yOut2 = yIn2 + half_dy;
-            int xIn3 = xOut2 + step_dx*gap;
-            int yIn3 = yOut2 + step_dy*gap;
+            int xIn3 = xOut2 + step_dx*1;
+            int yIn3 = yOut2 + step_dy*1;
             int xOut3 = xIn3 + half_dx;
             int yOut3 = yIn3 + half_dy;
-            int xIn4 = xOut3 - step_dy*sign*gap;
-            int yIn4 = yOut3 + step_dx*sign*gap;
+            int xIn4 = xOut3 - step_dy*sign*1;
+            int yIn4 = yOut3 + step_dx*sign*1;
             int xOut4 = xOut;
             int yOut4 = yOut;
             		
             drawCell(xIn1, yIn1, xOut1, yOut1, -sign);
-            path.lineTo(xIn2, yIn2);
+            extendPath(xIn2, yIn2);
             drawCell(xIn2, yIn2, xOut2, yOut2, sign);
-            path.lineTo(xIn3, yIn3);
+            extendPath(xIn3, yIn3);
             drawCell(xIn3, yIn3, xOut3, yOut3, sign);
-            path.lineTo(xIn4, yIn4);
+            extendPath(xIn4, yIn4);
             drawCell(xIn4, yIn4, xOut4, yOut4, -sign);
         }
     }
@@ -193,7 +277,7 @@ public class DrawView extends View {
 			}
 		}
         int pixelCount = width*height;
-        int size = gap;
+        int size = 1;
         int intensity = 0;
         int pixelsPassed = 0;
         for (int i = 0; i < levelCount; i++) {
@@ -227,7 +311,7 @@ public class DrawView extends View {
         File[] files = f.listFiles();
 
         if (files.length > 0) {
-			return files[0].getAbsolutePath();
+			return files[files.length-1].getAbsolutePath();
 		}
         return null;
     }   
